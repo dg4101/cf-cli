4375fb3aa731f43e79fee5785871a6e29090e1a9
dg4101 <dg4101ny@yahoo.co.jp>
2020-03-14 18:50:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh -l
-echo Starting cf-cli container
-#tail -f /dev/null
-/usr/bin/cf --version
+pwd;
+git --version;
+git clone https://github.com/dg4101/covid19.git;
+cd ./covid19;
+git checkout dev-scp;
+
+echo ====================;
+cf --version;
+
+cf api "$INPUT_CF_API"
+cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+
+if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+fi
+
+sh -c "$*"
